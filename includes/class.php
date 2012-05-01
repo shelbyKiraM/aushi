c7e316cc5eb610fbba4db6d68b602d2f29dee2a8
Shelby Munsch <smunsch@gmail.com>
2012-04-30 18:51:31 -0700

    Beginning to use objects for posts and other such things.

---
+++ includes/class.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Classes
+ *
+ * @author Shelby Munsch
+ * @version Pre-Alpha (0.1)
+ * @copyright Shelby Munsch, 30 April, 2012
+ * @package default
+ **/
+
+ /**
+  * undocumented class
+  *
+  * @package default
+  * @author Shelby Munsch
+  **/
+ class post
+ {
+	 public static 
+ } // END class 
